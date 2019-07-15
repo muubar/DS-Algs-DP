c9b6a177e6354dd198d1140e43aa1944940f42cf
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-07-15 19:06:00 +0200

    implement hash table using linked lists instead

--- hashTable.js
+++ hashTable.js
@@ -1,59 +1,47 @@
-//shallow hash function
+const linkedList = require("./linkedList.js");
+
 function hash(string, max) {
   var hash = 0;
   for (var i = 0; i < string.length; i++) {
     hash += string.charCodeAt(i);
   }
   return hash % max;
-};
-
+}
 
 class HashTable {
   constructor() {
-    this.STORAGE_LIMIT = 4;
-    this.storage = Array(this.STORAGE_LIMIT).fill(undefined);
+    this.SIZE_MAX = 4;
+    this.arr = Array(this.SIZE_MAX).fill(null);
   }
 
-  add(key, value) {
-    const idx = hash(key, this.STORAGE_LIMIT);
-    const obj = { key, value };
-
-    if (this.storage[idx]) {
-      //collision detected, checking if the passed key already exists, if it does, overwrite it
-      for (let i = 0; i < this.storage[idx].length; i++) {
-        if (this.storage[idx][i].key === key) {
-          this.storage[idx][i] = obj;
-          return obj;
-        }
-      }
-      //key not found, it was an honest collision
-      return this.storage[idx].push(obj);
+  add(value) {
+    const i = hash(value, this.SIZE_MAX);
+    var currentIndexLL = this.arr[i];
+    if (currentIndexLL) {
+      if (currentIndexLL.search(value)) return true;
+      currentIndexLL.add(value);
+      return true;
     }
-
-    else {
-      this.storage[idx] = [obj];
-      return obj;
-    }
+    this.arr[i] = new linkedList();
+    this.arr[i].add(value);
+    return true;
   }
 
-
-  remove(key) {
-    const idx = hash(key, this.STORAGE_LIMIT);
-    for (let i = 0; i < this.storage[idx].length; i++) {
-      if (storage[idx][i].key === key) {
-        return delete storage[idx][i];
-      }
+  has(value) {
+    const i = hash(value, this.SIZE_MAX);
+    if (this.arr[i]) {
+      return this.arr[i].search(value);
     }
     return false;
   }
 
-
-  search(key) {
-    const idx = hash(key, this.STORAGE_LIMIT);
-    for (let i = 0; i < this.storage[idx].length; i++) {
-      if (this.storage[idx][i].key === key) {
-        return this.storage[idx][i];
+  remove(value) {
+    const i = hash(value, this.SIZE_MAX);
+    if (this.arr[i]) {
+      for (var nodeVal of this.arr[i]) {
+        if (nodeVal === value) return this.arr[i].delete(value);
       }
+      return false;
     }
     return false;
   }
