580e193f170bb8b0cc9e23ba2942ac9666c2a133
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-07-27 16:41:13 +0200

    implement iterative binary search

--- binarySearch.js
+++ binarySearch.js
@@ -5,3 +5,19 @@ function binarySearch(elem, arr) {
   if (elem < arr[middle]) return binarySearch(elem, arr.slice(0, middle));
   return binarySearch(elem, arr.slice(middle + 1));
 }
+
+
+function binarySearchIterative(arr, val) {
+  var start = 0;
+  var end = arr.length - 1
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+    if (arr[middle] === val) return middle;
+    if (arr[middle] > val) {
+      end = middle - 1;
+      continue;
+    }
+    start = middle + 1;
+  }
+  return false;
+}
