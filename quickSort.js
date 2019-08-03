a8fc7fc9a2c0dab6c1d6d968f16b962aa0e16b52
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2019-08-03 16:22:15 +0200

    properly implement partition function

--- quickSort.js
+++ quickSort.js
@@ -1,30 +1,33 @@
 function quickSort(arr, start = 0, end = arr.length - 1) {
-  if (start < end) {
-    const pivot = mot(start, end);
-    partition(pivot);
+  if (end > start) {
+    const mid = mot(start, end);
+    const pivot = partition(mid, start, end);
     quickSort(arr, start, pivot - 1);
     quickSort(arr, pivot + 1, end);
     return arr;
   }
+  return arr;
 
 
-  function partition(pivot) {
-    swap(pivot, end);
-    var current = start;
-    for (let i = start; i < end - 1; i++) {
+  function partition(mid, start, end) {
+    swap(end, mid);
+    var swapIdx = start - 1;
+
+    for (let i = start; i < end; i++) {
       if (arr[i] <= arr[end]) {
-        swap(i, current);
-        current++;
+        swapIdx++;
+        swap(i, swapIdx);
       }
     }
-    swap(end, current);
+    swap(end, swapIdx + 1);
+    return swapIdx + 1;
   }
 
   function mot(start, end) {
-    const mid = Math.floor((start + end) / 2);
-    if (arr[start] > arr[mid]) swap(start, mid)
-    if (arr[start] > arr[end]) swap(start, end)
-    if (arr[mid] > arr[end]) swap(mid, end)
+    const mid = Math.ceil((start + end) / 2);
+    if (arr[start] > arr[mid]) swap(start, mid);
+    if (arr[mid] > arr[end]) swap(mid, end);
+    if (arr[start] > arr[end]) swap(start, end);
     return mid;
   }
 
