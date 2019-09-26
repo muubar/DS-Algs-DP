a308a9c6ae42365c4beb356a1876184af6d1dd5c
Ahmed <ahmedmudhar@hotmail.com>
2019-09-26 14:58:51 +0200

    improve implementation of observer pattern

--- observer.js
+++ observer.js
@@ -1,38 +1,34 @@
-class observable {
-  constructor() {
-    this.observersList = [];
+class Observer {
+  constructor(name) {
+    this.name = name;
+    this.observable = null;
   }
 
-  subscribe(...observer) {
-    this.observersList.push(...observer);
+  update(str) {
+    console.log(`${this.name} recieved: ${str}`);
+  }
+}
+
+class Observable {
+  constructor() {
+    this.observers = new Map();
+  }
+
+  subscribe(observer) {
+    if (this.observers.has(observer.name)) return false;
+    this.observers.set(observer, true);
+    return true;
   }
 
   unsubscribe(observer) {
-    const i = this.observersList.indexOf(observer);
-    if (i !== -1) this.observersList.splice(i, 1);
+    if (!this.observers.has(observer)) return false;
+    this.observers.delete(observer);
+    return true;
   }
 
-  notifyObservers() {
-    this.observersList.forEach((observer) => observer.notify());
-  }
-}
-
-
-class observer {
-  constructor(num) {
-    this.num = num;
-  }
-
-  notify() {
-    console.log(`observer ${this.num} notified!`);
+  update(str) {
+    for (let [key, value] of this.observers) {
+      key.update(str);
+    }
   }
 }
-
-var subject = new observable();
-var listener1 = new observer(1);
-var listener2 = new observer(2);
-var listener3 = new observer(3);
-
-subject.subscribe(listener1, listener2, listener3);
-subject.unsubscribe(listener2);
-subject.notifyObservers();
