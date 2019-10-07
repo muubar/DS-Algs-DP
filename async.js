9910da7b02f688ceb6baa4b502d39a892dd1557b
Ahmed <ahmedmudhar@hotmail.com>
2019-10-07 19:55:07 +0200

    add setTimeout to introduce async behaviour

--- async.js
+++ async.js
@@ -1,11 +1,12 @@
 //generator + promise with runner pattern
 function* gen() {
-  const promiseVal = yield new Promise((resolve) => resolve(42));
+  const promiseVal = yield new Promise((resolve) => setTimeout(() => resolve(5), 2000));
   console.log(promiseVal);
   const val = yield "hello";
   console.log(val);
-  const promiseVal2 = yield new Promise((resolve) => resolve(13));
+  const promiseVal2 = yield new Promise((resolve) => setTimeout(() => resolve(10), 2000));
   console.log(promiseVal2);
+  return promiseVal + promiseVal2;
 }
 
 function runner(genFn) {
@@ -14,16 +15,18 @@ function runner(genFn) {
 
   function run(arg) {
     const result = iterator.next(arg);
-    if (result.done) return result.value;
+    if (result.done) return Promise.resolve(result.value);
     return Promise.resolve(result.value).then(run);
   }
 }
 
-runner(gen);
+runner(gen).then(console.log);
+
+
 
 //async await
 async function equivalent() {
-  const promiseVal = await new Promise((resolve) => resolve(42));
+  const promiseVal = await new Promise((resolve) => setTimeout(() => resolve(5), 2000));
   const val = await promiseVal + 1;
   return val;
 }
